76143498a1d8ce57009f3848d252140f2f1ecc54
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-02-17 13:48:58 +0530

    Add files via upload

---
+++ JavaScript/Calculate_Square_&_Cubes_Upto_20/Calculate_Square_&_Cubes_Upto_20.js
@@ -0,0 +1,5 @@
+for (let i = 1; i < 11; i++) {
+  const squareofnum = Math.pow(i, 2);
+  const cubeofnum = Math.pow(i, 3);
+  console.log(`Square of ${i} is ${squareofnum} & it's is ${cubeofnum}`)
+}
---
+++ JavaScript/Calculate_Square_&_Cubes_Upto_20/Readme.md
@@ -0,0 +1,4 @@
+# Calculate Square & Cubes Upto 20
+
+## About Script
+This script calculates the Squares and Cubes of the numbers from 1 To 20.
